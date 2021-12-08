b18ca8b77bf2abad524f2dbf1403b8ab89e13cde
Ido Polak <Idop93@gmail.com>
2021-12-08 05:40:15 +0200

    homework 7

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 
-
-
 @app.route('/')
 def hello_world():  # put application's code here
     return render_template('index.html')
